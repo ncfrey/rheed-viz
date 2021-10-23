c20391a32950adcc57cf529d41fc964f369f3c47
William Chan <40780153+chan-w@users.noreply.github.com>
2021-10-23 18:51:40 -0500

    #7: Add changes to app.py to align with database

--- app.py
+++ app.py
@@ -2,13 +2,45 @@ import sys
 from pathlib import Path
 from molar import ClientConfig, Client
 
-admin_cfg = ClientConfig(server_url="http://localhost:8000",
-                         email="default@rheed.com",
-                         password="rheed",
+# needs to correspond to molarcli install local parameters
+local_address = "http://localhost:8000"
+default_admin_name = "default"
+default_admin_pw = "rheed"
+default_domain = "rheed.com"
+# other params
+default_user_name = "user1"
+
+# name the database here
+database_name = "compchem"
+revision_to_use = database_name + "@head"
+
+# create default admin config
+admin_cfg = ClientConfig(server_url=local_address,
+                         email=default_admin_name + "@" + default_domain,
+                         password=default_admin_pw,
                          database_name="main")
+
+# create default user config
+user_cfg = ClientConfig(server_url="http://localhost:8000",
+                        email=default_user_name + "@" + default_domain,
+                        password=default_admin_pw,
+                        database_name=database_name)
+
+
+# admin_cfg = ClientConfig(server_url="http://localhost:8000",
+#                          email="default@rheed.com",
+#                          password="rheed",
+#                          database_name="main")
 admin_client = Client(admin_cfg)
 
 admin_client.test_token()
+
+# user_cfg = ClientConfig(server_url="http://localhost:8000",
+#                         email="user1@molar.org",
+#                         password="rheed",
+#                         database_name="compchem")
+
+user_client = Client(user_cfg)
 
 file = Path(__file__).resolve()
 parent, root = file.parent, file.parents[1]
@@ -27,6 +59,7 @@ VERSION = ".".join(st.__version__.split(".")[:2])
 previous_version = "0.83.0"
 demo_pages = {
     "Session State": 0,
+    "Analysis": 1
 }
 
 st.set_page_config(page_title=f"New features in Streamlit {VERSION}")
@@ -62,6 +95,8 @@ def draw_main_page():
     st.write(release_notes)
 
     st.write(admin_client.test_token())
+    st.write(user_client.test_token())
+    st.write(user_client.get_database_information())
 
 
 # Draw sidebar
