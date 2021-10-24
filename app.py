75a4072f815d7e49deacfd1e7f9f639481b5bd69
William Chan <40780153+chan-w@users.noreply.github.com>
2021-10-24 02:41:27 -0500

    Change text on frontend to match rheed-viz

--- app.py
+++ app.py
@@ -105,17 +105,18 @@ def draw_analysis_page():
 
 previous_version = "0.83.0"
 demo_pages = {
-    "Session State": lambda: st.write(st.session_state),
-    "Manage Data": draw_manage_data_page,
-    "Analysis": draw_analysis_page
-}
+    "Analyze Data": draw_analysis_page,
+    "Manage Data": draw_manage_data_page
+}   
+demo_pages["View Session State"] = lambda: st.write(st.session_state)
 
-st.set_page_config(page_title=f"New features in Streamlit {VERSION}")
 
-contributors = []
+st.set_page_config(page_title="rheed-viz") # f"New features in Streamlit {VERSION}")
+
+contributors = ["Chris Price", "Nathan Frey", "Nathaniel Budijono", "William Chan"]
 
 intro = f"""
-This release launches session state as well as bug fixes and improvements.
+A data analysis and visualization platform for reflection high-energy electron diffraction (RHEED) data, powered by the MOLAR database.
 """
 
 release_notes = f"""
@@ -134,19 +135,19 @@ release_notes = f"""
 def draw_main_page():
     st.write(
         f"""
-        # Welcome to Streamlit {VERSION}! 👋
+        # Welcome to rheed-viz! 👋
         """
     )
 
     st.write(intro)
 
-    st.write(release_notes)
-    st.write(st.session_state)
+    # st.write(release_notes)
+    # st.write(st.session_state)
 
 
-    st.write(admin_client.test_token())
-    st.write(user_client.test_token())
-    st.write(user_client.get_database_information())
+    # st.write(admin_client.test_token())
+    # st.write(user_client.test_token())
+    # st.write(user_client.get_database_information())
 
 
 
@@ -156,8 +157,8 @@ def draw_main_page():
 pages = list(demo_pages.keys())
 
 if len(pages):
-    pages.insert(0, "Release Notes")
-    st.sidebar.title(f"Streamlit v{VERSION} Demos")
+    pages.insert(0, "About")
+    st.sidebar.title("Menu") # f"Streamlit v{VERSION} Demos")
     query_params = st.experimental_get_query_params()
     if "page" in query_params and query_params["page"][0] == "headliner":
         idx = 1
