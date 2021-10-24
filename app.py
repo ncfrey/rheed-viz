7dc43f9462344d64cf29eaf7ecf32f18aba1bea8
Nathaniel Budijono <nathanielbd@gmail.com>
2021-10-24 04:24:41 -0500

    add landing page, plot page, and add video upload+view

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from pandas.core import frame
 import numpy as np
 from PIL import Image
 import skimage
+import plotly.express as px
 
 # needs to correspond to molarcli install local parameters
 local_address = "http://557d-198-232-127-62.ngrok.io"
@@ -85,6 +86,12 @@ def draw_manage_data_page():
         for idx, file in enumerate(uploaded_files):
             process_image(file)
             process_progress_bar.progress((idx + 1) / len(uploaded_files))
+    uploaded_video = st.file_uploader("Choose mp4 files", type="mp4", accept_multiple_files=True)
+    video_bar = st.progress(0)
+    with st.spinner("Processing files..."):
+        for idx, file in enumerate(uploaded_video):
+            st.video(file.getvalue())
+            video_bar.progress((idx + 1) / len(uploaded_video))
 
 
 
@@ -99,11 +106,49 @@ def draw_analysis_page():
     selected_image = get_image_from_raw(raw_images["x"][selected_frame], raw_images["metadata"][selected_frame]["shape"])
     st.image(selected_image, clamp=True)
 
+def draw_plot_page():
+    st.title("Plots")
+
+    is_pca = [conf["data_type"] == "pca_time" for conf in all_confomers["metadata"]]
+    pca_data = all_confomers[is_pca]
+
+    pc_to_col = {
+        1: 'x',
+        2: 'y',
+        3: 'z'
+    }
+
+    st.header("Principal components over time")
+    pc = st.select_slider("Principal component", options=pc_to_col.keys())
+    fig = px.scatter(
+        x=range(len(pca_data[pc_to_col[pc]][7])),
+        y=pca_data[pc_to_col[pc]][7],
+        labels={
+            'y': f'PC{pc}',
+            'x': 'Frame number'
+        }
+    )
+    st.plotly_chart(fig)
+    st.write(f'Spike at frame {np.argmax(np.abs(np.gradient(pca_data[pc_to_col[pc]][7])))}')
+    
+def draw_landing_page():
+    content = f"""
+    # RHEED Analysis Platform
+    *Background and problem.* Reflection high-energy electron diffraction (RHEED) is a popular technique for monitoring the growth of high-quality thin films during molecular beam epitaxy (MBE), a fundamental technique in nanotechnology development. Researchers use tedious, manual tools to analyze RHEED data. Current workflows are slow, non-transferable, and not reproducible.
+
+    *Aim 1.* Develop a prototype interface where users can upload RHEED images and organize them in a Molar database.
+
+    *Aim 2.* Apply pre-processing and dimensionality reduction so users can quickly and easily analyze their data.
+    """
+    st.write(content)
+
 previous_version = "0.83.0"
 demo_pages = {
-    "Session State": lambda: st.write(st.session_state),
+    "About": draw_landing_page,
+    # "Session State": lambda: st.write(st.session_state),
     "Manage Data": draw_manage_data_page,
-    "Analysis": draw_analysis_page
+    "Analysis": draw_analysis_page,
+    "Plots": draw_plot_page
 }
 
 st.set_page_config(page_title=f"New features in Streamlit {VERSION}")
@@ -144,27 +189,24 @@ def draw_main_page():
     st.write(user_client.test_token())
     st.write(user_client.get_database_information())
 
-
-
-
-
 # Draw sidebar
 pages = list(demo_pages.keys())
 
 if len(pages):
-    pages.insert(0, "Release Notes")
-    st.sidebar.title(f"Streamlit v{VERSION} Demos")
+    # pages.insert(0, "Release Notes")
+    # st.sidebar.title(f"Streamlit v{VERSION} Demos")
+    st.sidebar.title("Menu")
     query_params = st.experimental_get_query_params()
     if "page" in query_params and query_params["page"][0] == "headliner":
         idx = 1
     else:
         idx = 0
     selected_demo = st.sidebar.radio("", pages, idx)
-else:
-    selected_demo = "Release Notes"
+# else:
+#     selected_demo = "Release Notes"
 
 # Draw main page
 if selected_demo in demo_pages:
     demo_pages[selected_demo]()
-else:
-    draw_main_page()
+# else:
+#     draw_main_page()
